8bec528456956522a2e7f1d6c0705504e1036557
SchoolTran <truongnulo1709@gmail.com>
2024-05-09 20:58:47 +0700

    up bai 4

---
+++ Bai4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>();
+
+            studentList.Add(new Student("Alice", 20));
+            studentList.Add(new Student("Bod", 22));
+            studentList.Add(new Student("Charlie", 21));
+
+            foreach (var student in studentList)
+            {
+                Console.WriteLine(student.DisplayInfo());
+            }
+            Console.ReadKey();
+        }
+    }
+}
